b4c1ee5b81437af282c8150e5b0af249918bdd48
Arseniy <dormer_awe_01@icloud.com>
2023-01-25 10:33:36 -0800

    Initial Commit

---
+++ Aurora Tracker/Aurora_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aurora_TrackerApp.swift
+//  Aurora Tracker
+//
+//  Created by Arseniy Churanov on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Aurora_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
